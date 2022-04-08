0c766e0c91056d4f02e8895fa1864fcf7eb8f340
Steve Flanagan <flanagan.s@husky.neu.edu>
2022-04-08 11:52:55 -0400

    please the prettier gods

--- contracts/test/utils/AllowListHelper.sol
+++ contracts/test/utils/AllowListHelper.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.10;
 
 library AllowlistHelper {
-    function batchAllowees(uint256 length) public pure returns(bool[] memory) {
+    function batchAllowees(uint256 length) public pure returns (bool[] memory) {
         bool[] memory statuses = new bool[](length);
         for (uint256 i = 0; i < length; i++) {
             statuses[i] = true;
