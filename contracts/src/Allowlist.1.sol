241a74269de2f0c564171c0c7db1de1e2592cb46
Iulian Rotaru <iulian@rotaru.fr>
2022-05-04 11:27:14 +0200

    feat: split allowlist out of river
    - remove allowlist manager and move logic to allowlist standalone
      contract
    - rewire river allowlist calls to external contract

--- contracts/src/Allowlist.1.sol
+++ contracts/src/Allowlist.1.sol
@@ -1,20 +1,29 @@
 //SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.10;
 
-import "../libraries/Errors.sol";
+import "./Initializable.sol";
+import "./libraries/Errors.sol";
+import "./libraries/LibOwnable.sol";
+import "./interfaces/IRiverOracleInput.sol";
 
-import "../state/shared/AdministratorAddress.sol";
-import "../state/river/AllowerAddress.sol";
-import "../state/river/Allowlist.sol";
-import "../libraries/LibOwnable.sol";
+import "./state/allowlist/AllowerAddress.sol";
+import "./state/allowlist/Allowlist.sol";
 
-/// @title Allowlist Manager (v1)
+/// @title Allowlist (v1)
 /// @author SkillZ
-/// @notice This contract handles the allowlist of accounts allowed to own shares
-abstract contract AllowlistManagerV1 {
+/// @notice This contract handles the input from the allowed oracle members. Highly inspired by Lido's implementation.
+contract AllowlistV1 is Initializable {
     error InvalidAlloweeCount();
     error MismatchedAlloweeAndStatusCount();
     event ChangedAllowlistStatuses(address[] indexed accounts, uint256[] statuses);
+
+    /// @notice Initializes the allowlist
+    /// @param _admin Address of the Allowlist administrator
+    /// @param _allower Address of the allower
+    function initAllowlistV1(address _admin, address _allower) external init(0) {
+        LibOwnable._setAdmin(_admin);
+        AllowerAddress.set(_allower);
+    }
 
     /// @notice Prevents unauthorized calls
     modifier onlyAdmin() virtual {
@@ -22,12 +31,6 @@ abstract contract AllowlistManagerV1 {
             revert Errors.Unauthorized(msg.sender);
         }
         _;
-    }
-
-    /// @notice Initializes the allower address
-    /// @param _allowerAddress Address allowed to edit the allowlist
-    function initAllowlistManagerV1(address _allowerAddress) internal {
-        AllowerAddress.set(_allowerAddress);
     }
 
     /// @notice Changes the allower address
