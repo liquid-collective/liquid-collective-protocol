6ae92d559970fa1ba7745765b04a7cb2a46bcc39
Iulian Rotaru <mortimr@Iulians-MacBook-Pro.local>
2022-09-01 18:47:39 +0200

    feat: update artifact generator to add firewall abis

--- gen_artifacts.ts
+++ gen_artifacts.ts
@@ -1,6 +1,8 @@
 import { writeFileSync } from "fs";
 import hre from "hardhat";
 
+const firewalledContract = ["RiverV1", "AllowlistV1", "OracleV1", "OperatorsRegistryV1"];
+
 async function main() {
   const network = hre.network;
   if (network.name === "hardhat") {
@@ -12,11 +14,31 @@ async function main() {
   const artifactContent = require(`./${artifactName}`);
   const namedAccounts = await hre.getNamedAccounts();
   const contractNames = Object.keys(artifactContent.contracts);
+  let firewallAbi;
+  const contractsAbis = {};
   for (const contractName of contractNames) {
+    if (firewalledContract.includes(contractName)) {
+      contractsAbis[contractName] = artifactContent.contracts[contractName].abi;
+    }
+    if (contractName === "Firewall") {
+      firewallAbi = artifactContent.contracts[contractName].abi;
+    }
     artifactContent.contracts[contractName] = artifactContent.contracts[contractName].address;
+  }
+  const firewalledContractNames = Object.keys(contractsAbis);
+  const firewallAbisFileContent = {};
+  for (const firewalled of firewalledContractNames) {
+    let baseAbi = [...firewallAbi];
+    for (const element of contractsAbis[firewalled]) {
+      if (element.stateMutability !== "view" && element.stateMutability !== "pure") {
+        baseAbi = [...baseAbi, element];
+      }
+    }
+    firewallAbisFileContent[firewalled] = baseAbi;
   }
   artifactContent.namedAccounts = namedAccounts;
   writeFileSync(artifactName, JSON.stringify(artifactContent, null, 4));
+  writeFileSync(`firewall_abis.${network.name}.json`, JSON.stringify(firewallAbisFileContent, null, 4));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
