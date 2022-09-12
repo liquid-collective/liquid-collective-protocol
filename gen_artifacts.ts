964d22c7dad6b46006d2fea4eb5db44181c6d119
mortimr <iulian@rotaru.fr>
2022-09-12 19:00:54 +0200

    feat: update artifact generator to add firewall abis (#115)
    * feat: update artifact generator to add firewall abis
    
    * feat: move artifact destination to deployments directory
    
    Co-authored-by: Iulian Rotaru <mortimr@Iulians-MacBook-Pro.local>

--- gen_artifacts.ts
+++ gen_artifacts.ts
@@ -1,5 +1,8 @@
-import { writeFileSync } from "fs";
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import hre from "hardhat";
+import { join } from "path";
+
+const firewalledContract = ["RiverV1", "AllowlistV1", "OracleV1", "OperatorsRegistryV1"];
 
 async function main() {
   const network = hre.network;
@@ -12,8 +15,31 @@ async function main() {
   const artifactContent = require(`./${artifactName}`);
   const namedAccounts = await hre.getNamedAccounts();
   const contractNames = Object.keys(artifactContent.contracts);
+  let firewallAbi;
+  const contractsAbis = {};
   for (const contractName of contractNames) {
+    if (firewalledContract.includes(contractName)) {
+      contractsAbis[contractName] = artifactContent.contracts[contractName].abi;
+    }
+    if (contractName === "Firewall") {
+      firewallAbi = artifactContent.contracts[contractName].abi;
+    }
     artifactContent.contracts[contractName] = artifactContent.contracts[contractName].address;
+  }
+  const firewalledContractNames = Object.keys(contractsAbis);
+  for (const firewalled of firewalledContractNames) {
+    let baseAbi = [...firewallAbi];
+    for (const element of contractsAbis[firewalled]) {
+      if (element.stateMutability !== "view" && element.stateMutability !== "pure") {
+        baseAbi = [...baseAbi, element];
+      }
+    }
+    const dirName = `deployments/${network.name}/firewallAbis`;
+    if (!existsSync(dirName)) {
+      mkdirSync(dirName);
+    }
+    const fileName = join(dirName, `${firewalled}.abi.json`);
+    writeFileSync(fileName, JSON.stringify(baseAbi, null, 4));
   }
   artifactContent.namedAccounts = namedAccounts;
   writeFileSync(artifactName, JSON.stringify(artifactContent, null, 4));
