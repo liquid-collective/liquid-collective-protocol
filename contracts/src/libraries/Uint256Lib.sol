0b0a3ade4e561a53a45c140f54fac16893ddc78c
mortimr <iulian@rotaru.fr>
2022-09-20 20:02:06 +0200

    [SPEARBIT/25] River gas optimisations

--- contracts/src/libraries/Uint256Lib.sol
+++ contracts/src/libraries/Uint256Lib.sol
@@ -5,13 +5,29 @@ library Uint256Lib {
     function toLittleEndian64(uint256 _value) internal pure returns (uint256 result) {
         result = 0;
         uint256 temp_value = _value;
-        for (uint256 i = 0; i < 8;) {
-            result = (result << 8) | (temp_value & 0xFF);
-            temp_value >>= 8;
-            unchecked {
-                ++i;
-            }
-        }
+        result = temp_value & 0xFF;
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
+
+        result = (result << 8) | (temp_value & 0xFF);
+        temp_value >>= 8;
 
         assert(0 == temp_value); // fully converted
         result <<= (24 * 8);
