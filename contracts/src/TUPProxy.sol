6eff415321f416a91da93ef0fedacec051e3fe43
Iulian Rotaru <iulian@rotaru.fr>
2022-09-28 18:22:57 +0200

    feat: change tupproxy pause slot id

--- contracts/src/TUPProxy.sol
+++ contracts/src/TUPProxy.sol
@@ -8,7 +8,7 @@ import "openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeabl
 /// @notice This contract extends the Transparent Upgradeable proxy and adds a system wide pause feature.
 ///         When the system is paused, the fallback will fail no matter what calls are made.
 contract TUPProxy is TransparentUpgradeableProxy {
-    bytes32 private constant _PAUSE_SLOT = bytes32(uint256(keccak256("eip1967.proxy.pause")) - 1);
+    bytes32 private constant _PAUSE_SLOT = bytes32(uint256(keccak256("river.tupproxy.pause")) - 1);
 
     error CallWhenPaused();
 
