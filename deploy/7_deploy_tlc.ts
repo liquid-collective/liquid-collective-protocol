1dcde3686b36bcdda97642ee724a9621ef4a88fc
mortimr <iulian@rotaru.fr>
2022-10-21 18:38:03 +0200

    chore(staging): deploy tlc contract (#159)
    * chore(staging): deploy tlc contract
    
    * fix: rename alluvialTreasury in tlcMintAccount
    
    * fix: redeploy staging and dev with proxy firewalls

--- deploy/7_deploy_tlc.ts
+++ deploy/7_deploy_tlc.ts
@@ -1,24 +1,47 @@
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { isDeployed, logStep, logStepEnd } from "../ts-utils/helpers/index";
+import { getContractAddress } from "ethers/lib/utils";
 
-const func: DeployFunction = async function ({ deployments, getNamedAccounts }: HardhatRuntimeEnvironment) {
-  const { deployer, proxyAdministrator, alluvialTreasury } = await getNamedAccounts();
+const func: DeployFunction = async function ({ deployments, getNamedAccounts, ethers }: HardhatRuntimeEnvironment) {
+  const { deployer, executor, proxyAdministrator, tlcMintAccount } = await getNamedAccounts();
 
-  await deployments.deploy("TLC", {
+  const signer = await ethers.getSigner(deployer);
+
+  const txCount = await signer.getTransactionCount();
+  const futureTLCAddress = getContractAddress({
+    from: deployer,
+    nonce: txCount + 2, // proxy is in 8 txs
+  });
+
+  const proxyArtifact = await deployments.getArtifact("TUPProxy");
+  const proxyInterface = new ethers.utils.Interface(proxyArtifact.abi);
+
+  const tlcProxyFirewallDeployment = await deployments.deploy("TLCProxyFirewall", {
+    contract: "Firewall",
+    from: deployer,
+    log: true,
+    args: [proxyAdministrator, executor, futureTLCAddress, [proxyInterface.getSighash("pause()")]],
+  });
+
+  const tlcDeployment = await deployments.deploy("TLC", {
     contract: "TLCV1",
     from: deployer,
     log: true,
     proxy: {
-      owner: proxyAdministrator,
+      owner: tlcProxyFirewallDeployment.address,
       proxyContract: "TUPProxy",
       implementationName: "TLCV1_Implementation",
       execute: {
         methodName: "initTLCV1",
-        args: [alluvialTreasury],
+        args: [tlcMintAccount],
       },
     },
   });
+
+  if (tlcDeployment.address !== futureTLCAddress) {
+    throw new Error(`Invalid future tlc address computation ${futureTLCAddress} != ${tlcDeployment.address}`);
+  }
 
   logStepEnd(__filename);
 };
