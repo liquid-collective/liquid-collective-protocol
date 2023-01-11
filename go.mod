0347a33e0e9237488ce9a89ef1b488bdf683eea1
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2023-01-11 22:54:12 +0100

    create tooling to upload contracts to Tenderly

---
+++ go.mod
@@ -0,0 +1,46 @@
+module github.com/liquid-collective/liquid-collective-protocol
+
+go 1.18
+
+require (
+	github.com/tenderly/tenderly-cli v1.4.10
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/badoux/checkmail v0.0.0-20181210160741-9661bd69e9ad // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
+	github.com/ethereum/go-ethereum v1.10.8 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-github/v37 v37.0.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/holiman/uint256 v1.2.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/palantir/pkg v1.0.1 // indirect
+	github.com/palantir/pkg/datetime v1.0.1 // indirect
+	github.com/palantir/pkg/safejson v1.0.1 // indirect
+	github.com/palantir/pkg/safeyaml v1.0.1 // indirect
+	github.com/palantir/pkg/transform v1.0.0 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.10.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
+
+replace github.com/tenderly/tenderly-cli v1.4.10 => github.com/liquid-collective/tenderly-cli v0.0.0-20230116221706-05be5ab46c44
---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/tenderly/tenderly-cli/config"
+	"github.com/tenderly/tenderly-cli/hardhat"
+	"github.com/tenderly/tenderly-cli/providers"
+	"github.com/tenderly/tenderly-cli/rest"
+	"github.com/tenderly/tenderly-cli/rest/call"
+	"github.com/tenderly/tenderly-cli/rest/payloads"
+	"gopkg.in/yaml.v2"
+)
+
+func init() {
+	config.Init()
+}
+
+func NewRest() *rest.Rest {
+	return rest.NewRest(
+		call.NewAuthCalls(),
+		call.NewUserCalls(),
+		call.NewProjectCalls(),
+		call.NewContractCalls(),
+		call.NewExportCalls(),
+		call.NewNetworkCalls(),
+		call.NewActionCalls(),
+	)
+}
+
+type ContractABI struct {
+	Path string `yaml:"path"`
+	Name string `yaml:"name"`
+	Tag  string `yaml:"tag"`
+}
+
+func UploadContract(cl *rest.Rest, abi *ContractABI, projectSlug, networkID string) error {
+	fmt.Printf("Upload contract %v\n", abi)
+
+	// read ABI file
+	b, err := os.ReadFile(abi.Path)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal ABI in HardhatContract object
+	hardhatContract := new(hardhat.HardhatContract)
+	err = json.Unmarshal(b, hardhatContract)
+	if err != nil {
+		return err
+	}
+
+	// Unmarshal Metadata
+	var hardhatMeta providers.ContractMetadata
+	if hardhatContract.Metadata != "" {
+		err = json.Unmarshal([]byte(hardhatContract.Metadata), &hardhatMeta)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create upload contract request object
+	req := &payloads.UploadContractsRequest{
+		Config: &payloads.Config{
+			OptimizationsUsed:  func(b bool) *bool { return &b }(*hardhatMeta.Settings.Optimizer.Enabled),
+			OptimizationsCount: func(i int) *int { return &i }(*hardhatMeta.Settings.Optimizer.Runs),
+			EvmVersion:         func(s string) *string { return &s }(*hardhatMeta.Settings.EvmVersion),
+		},
+		Tag: abi.Tag,
+	}
+
+	version := fmt.Sprintf("v%v", strings.Split(hardhatMeta.Compiler.Version, "+")[0])
+
+	// Loop over contract sources found in metadata to populate the list of contract dependencies
+	for path, source := range hardhatMeta.Sources {
+		contract := providers.Contract{
+			Source:     source.Content,
+			SourcePath: path,
+			Compiler: providers.ContractCompiler{
+				Name:    "solc",
+				Version: version,
+			},
+		}
+
+		//
+		// When reaching the ultimate contract, attaches the network information and address of deployment
+		// TODO: CompilationTarget attribute has been manually added on nmvalera local dep. We should submit a PR to tenderly-cli to add it
+		if name, ok := hardhatMeta.Settings.CompilationTarget[path]; ok {
+			contract.Name = name
+			contract.Networks = make(map[string]providers.ContractNetwork)
+			contract.Networks[networkID] = providers.ContractNetwork{
+				Address: hardhatContract.Address,
+			}
+		}
+
+		req.Contracts = append(req.Contracts, contract)
+	}
+
+	resp, err := cl.Contract.UploadContracts(*req, projectSlug)
+	if err != nil {
+		return err
+	}
+
+	if resp.Error != nil {
+		return resp.Error
+	}
+
+	fmt.Printf("-> Contract uploaded: %v\n", resp.Contracts)
+
+	if abi.Name != "" {
+		renameReq := payloads.RenameContractRequest{
+			DisplayName: abi.Name,
+		}
+		resp, err := cl.Contract.RenameContract(renameReq, projectSlug, networkID, hardhatContract.Address)
+		if err != nil {
+			return err
+		}
+
+		if resp != nil && resp.Error != nil {
+			return resp.Error
+		}
+
+		fmt.Printf("-> Contract renamed to: %v\n", abi.Name)
+	}
+
+	return nil
+}
+
+type Projects struct {
+	Projects map[string]*Project `yaml:"projects"`
+}
+
+type Project struct {
+	Slug      string         `yaml:"slug"`
+	Network   string         `yaml:"network"`
+	Contracts []*ContractABI `yaml:"contracts"`
+}
+
+func main() {
+	cl := NewRest()
+
+	principal, err := cl.User.Principal()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Connected as %v\n", principal)
+
+	// read yaml file
+	b, err := os.ReadFile("./tenderly.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	projects := new(Projects)
+	err = yaml.Unmarshal(b, projects)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, project := range projects.Projects {
+		for _, contract := range project.Contracts {
+			err := UploadContract(cl, contract, project.Slug, project.Network)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
