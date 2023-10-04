ec5c2f0a9c2b92c1a967959920668dd710bc9120
Mehdi <cebidhem@pm.me>
2023-10-04 17:05:48 +0200

    update verify script (#229)
    Signed-off-by: Mehdi Bechiri <cebidhem@pm.me>

--- scripts/verify_deployment.sh
+++ scripts/verify_deployment.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 # Set RIVER env variable to the address of the river contract
+# Set RIVER_PROXY_FIREWALL env variable to the address of the river proxy firewall contract
+# Set ORACLE_PROXY_FIREWALL env variable to the address of the oracle proxy firewall contract
+# Set ALLOWLIST_PROXY_FIREWALL env variable to the address of the allowlist proxy firewall contract
+# Set OPERATORS_REGISTRY_PROXY_FIREWALL env variable to the address of the operators registry proxy firewall contract
+# Set REDEEM_MANAGER_PROXY_FIREWALL env variable to the address of the redeem manager proxy firewall contract
+# Set TLC_PROXY_FIREWALL env variable to the address of the tlc proxy firewall contract
 
 RIVER_FIREWALL=$(cast call ${RIVER} "getAdmin()(address)")
 echo "River admin is ${RIVER_FIREWALL}"
@@ -61,7 +67,6 @@ echo "River metadata uri is $(cast call ${RIVER} "getMetadataURI()(string)")"
 
 echo "River withdrawal credentials are $(cast call ${RIVER} "getWithdrawalCredentials()(bytes32)")"
 
-REDEEM_MANAGER_PROXY_FIREWALL="0x$(cast storage ${REDEEM_MANAGER} 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103 | cut -c 27-)"
 echo "Redeem Manager Proxy Firewall is ${REDEEM_MANAGER_PROXY_FIREWALL}"
 
 REDEEM_MANAGER_PROXY_FIREWALL_ADMIN=$(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "getAdmin()(address)")
@@ -70,31 +75,74 @@ echo "Redeem Manager Proxy Firewall Governor is ${REDEEM_MANAGER_PROXY_FIREWALL_
 REDEEM_MANAGER_PROXY_FIREWALL_EXECUTOR=$(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "executor()(address)")
 echo "Redeem Manager Proxy Firewall Executor is ${REDEEM_MANAGER_PROXY_FIREWALL_EXECUTOR}"
 
-echo "Operators Registry Executor permission to call addOperator = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "addOperator(string,address)" | head -c 10))"
-echo "Operators Registry Executor permission to call setOperatorAddress = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setOperatorAddress(uint256,address)" | head -c 10))"
-echo "Operators Registry Executor permission to call setOperatorName = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setOperatorName(uint256,string)" | head -c 10))"
-echo "Operators Registry Executor permission to call setOperatorStatus = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setOperatorStatus(uint256,bool)" | head -c 10))"
-echo "Operators Registry Executor permission to call setStoppedValidatorCount = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setOperatorStoppedValidatorCount(uint256,uint256)" | head -c 10))"
-echo "Operators Registry Executor permission to call setOperatorLimits  = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setOperatorLimits(uint256[],uint256[],uint256)" | head -c 10))"
-echo "Operators Registry Executor permission to call addValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "addValidators(uint256,uint256,bytes)" | head -c 10))"
-echo "Operators Registry Executor permission to call removeValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "removeValidators(uint256,uint256[])" | head -c 10))"
-echo "Operators Registry Executor permission to call pickNextValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "pickNextValidators(uint256)" | head -c 10))"
+echo "Operators Registry Executor permission to call addOperator = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "addOperator(string,address)"))"
+echo "Operators Registry Executor permission to call setOperatorAddress = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOperatorAddress(uint256,address)"))"
+echo "Operators Registry Executor permission to call setOperatorName = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOperatorName(uint256,string)"))"
+echo "Operators Registry Executor permission to call setOperatorStatus = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOperatorStatus(uint256,bool)"))"
+echo "Operators Registry Executor permission to call setOperatorLimits  = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOperatorLimits(uint256[],uint32[],uint256)"))"
+echo "Operators Registry Executor permission to call addValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "addValidators(uint256,uint32,bytes)"))"
+echo "Operators Registry Executor permission to call removeValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "removeValidators(uint256,uint256[])"))"
+echo "Operators Registry Executor permission to call pause = $(cast call ${OPERATORS_REGISTRY_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pause()"))"
+echo "Operators Registry Executor permission to call unpause = $(cast call ${OPERATORS_REGISTRY_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "unpause()"))"
+echo "Operators Registry Executor permission to call changeAdmin = $(cast call ${OPERATORS_REGISTRY_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "changeAdmin()"))"
+echo "Operators Registry Executor permission to call upgrade = $(cast call ${OPERATORS_REGISTRY_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgrade()"))"
+echo "Operators Registry Executor permission to call upgradeToAndCall = $(cast call ${OPERATORS_REGISTRY_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgradeToAndCall()"))"
 
-echo "Oracle Executor permission to call addMember = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "addMember(address,uint256)" | head -c 10))"
-echo "Oracle Executor permission to call removeMember = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "removeMember(address,uint256)" | head -c 10))"
-echo "Oracle Executor permission to call setMember = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setMember(address,address)" | head -c 10))"
-echo "Oracle Executor permission to call setCLSpec = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setCLSpec(uint64,uint64,uint64,uint64)" | head -c 10))"
-echo "Oracle Executor permission to call setReportBounds = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setReportBounds(uint256,uint256)" | head -c 10))"
-echo "Oracle Executor permission to call setQuorum = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setQuorum(uint256)" | head -c 10))"
-echo "Oracle Executor permission to call reportConsensusLayerData = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "reportConsensusLayerData(uint256,uint64,uint32)" | head -c 10))"
+echo "Oracle Executor permission to call addMember = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "addMember(address,uint256)"))"
+echo "Oracle Executor permission to call removeMember = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "removeMember(address,uint256)"))"
+echo "Oracle Executor permission to call setMember = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setMember(address,address)"))"
+echo "Oracle Executor permission to call setCLSpec = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setCLSpec(uint64,uint64,uint64,uint64)"))"
+echo "Oracle Executor permission to call setReportBounds = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setReportBounds(uint256,uint256)"))"
+echo "Oracle Executor permission to call setQuorum = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setQuorum(uint256)"))"
+echo "Oracle Executor permission to call reportConsensusLayerData = $(cast call ${ORACLE_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "reportConsensusLayerData(uint256,uint64,uint32)"))"
+echo "Oracle Executor permission to call pause = $(cast call ${ORACLE_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pause()"))"
+echo "Oracle Executor permission to call unpause = $(cast call ${ORACLE_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "unpause()"))"
+echo "Oracle Executor permission to call changeAdmin = $(cast call ${ORACLE_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "changeAdmin()"))"
+echo "Oracle Executor permission to call upgrade = $(cast call ${ORACLE_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgrade()"))"
+echo "Oracle Executor permission to call upgradeToAndCall = $(cast call ${ORACLE_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgradeToAndCall()"))"
 
-echo "Allowlist Executor permission to call setAllower = $(cast call ${ALLOWLIST_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setAllower(address)" | head -c 10))"
-echo "Allowlist Executor permission to call allow = $(cast call ${ALLOWLIST_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "allow(address[],uint256[])" | head -c 10))"
+echo "Allowlist Executor permission to call setAllower = $(cast call ${ALLOWLIST_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setAllower(address)"))"
+echo "Allowlist Executor permission to call allow = $(cast call ${ALLOWLIST_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "allow(address[],uint256[])"))"
+echo "Allowlist Executor permission to call pause = $(cast call ${ALLOWLIST_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pause()"))"
+echo "Allowlist Executor permission to call unpause = $(cast call ${ALLOWLIST_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "unpause()"))"
+echo "Allowlist Executor permission to call changeAdmin = $(cast call ${ALLOWLIST_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "changeAdmin()"))"
+echo "Allowlist Executor permission to call upgrade = $(cast call ${ALLOWLIST_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgrade()"))"
+echo "Allowlist Executor permission to call upgradeToAndCall = $(cast call ${ALLOWLIST_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgradeToAndCall()"))"
 
-echo "River Executor permission to call setGlobalFee = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setGlobalFee(uint256)" | head -c 10))"
-echo "River Executor permission to call setRiver = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setRiver(address)" | head -c 10))"
-echo "River Executor permission to call setCollector = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setCollector(address)" | head -c 10))"
-echo "River Executor permission to call setELFeeRecipient = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setELFeeRecipient(address)" | head -c 10))"
-echo "River Executor permission to call setOracle = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setOracle(address)" | head -c 10))"
-echo "River Executor permission to call setConsensusLayerData = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "setConsensusLayerData(uint256,uint256,bytes32,uint256)" | head -c 10))"
-echo "River Executor permission to call depositToConsensusLayer = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast keccak "depositToConsensusLayer(uint256)" | head -c 10))"
+echo "River Executor permission to call setGlobalFee = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setGlobalFee(uint256)"))"
+echo "River Executor permission to call setRiver = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setRiver(address)"))"
+echo "River Executor permission to call setCollector = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setCollector(address)"))"
+echo "River Executor permission to call setELFeeRecipient = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setELFeeRecipient(address)"))"
+echo "River Executor permission to call setOracle = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOracle(address)"))"
+echo "River Executor permission to call setConsensusLayerData = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setConsensusLayerData(uint256,uint256,uint256,uint256,uint256,uint32,uint32[],bool,bool)"))"
+echo "River Executor permission to call setDailyCommittableLimits = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setDailyCommittableLimits(uint128,uint128)"))"
+echo "River Executor permission to call setAllowlist = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setAllowlist(address)"))"
+echo "River Executor permission to call setCoverageFund = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setCoverageFund(address)"))"
+echo "River Executor permission to call setMetadataURI = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setMetadataURI(string)"))"
+echo "River Executor permission to call pause = $(cast call ${RIVER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pause()"))"
+echo "River Executor permission to call unpause = $(cast call ${RIVER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "unpause()"))"
+echo "River Executor permission to call changeAdmin = $(cast call ${RIVER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "changeAdmin()"))"
+echo "River Executor permission to call upgrade = $(cast call ${RIVER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgrade()"))"
+echo "River Executor permission to call upgradeToAndCall = $(cast call ${RIVER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgradeToAndCall()"))"
+
+echo "Redeem Manager Executor permission to call pause = $(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pause()"))"
+echo "Redeem Manager Executor permission to call unpause = $(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "unpause()"))"
+echo "Redeem Manager Executor permission to call changeAdmin = $(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "changeAdmin()"))"
+echo "Redeem Manager Executor permission to call upgrade = $(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgrade()"))"
+echo "Redeem Manager Executor permission to call upgradeToAndCall = $(cast call ${REDEEM_MANAGER_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgradeToAndCall()"))"
+
+echo "TLC Executor permission to call pause = $(cast call ${TLC_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pause()"))"
+echo "TLC Executor permission to call unpause = $(cast call ${TLC_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "unpause()"))"
+echo "TLC Executor permission to call changeAdmin = $(cast call ${TLC_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "changeAdmin()"))"
+echo "TLC Executor permission to call upgrade = $(cast call ${TLC_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgrade()"))"
+echo "TLC Executor permission to call upgradeToAndCall = $(cast call ${TLC_PROXY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "upgradeToAndCall()"))"
+
+# Legacy functions checks
+
+echo "Operators Registry Executor permission to call legacy setOperatorStoppedValidatorCount = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOperatorStoppedValidatorCount(uint256,uint256)"))"
+echo "Operators Registry Executor permission to call legacy setOperatorLimits  = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setOperatorLimits(uint256[],uint256[],uint256)"))"
+echo "Operators Registry Executor permission to call legacy addValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "addValidators(uint256,uint256,bytes)"))"
+echo "Operators Registry Executor permission to call legacy pickNextValidators = $(cast call ${OPERATORS_REGISTRY_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "pickNextValidators(uint256)"))"
+
+echo "River Executor permission to call legacy setConsensusLayerData = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "setConsensusLayerData(uint256,uint256,bytes32,uint256)"))"
+echo "River Executor permission to call legacy depositToConsensusLayer = $(cast call ${RIVER_FIREWALL} "executorCanCall(bytes4)(bool)" $(cast sig "depositToConsensusLayer(uint256)"))"
