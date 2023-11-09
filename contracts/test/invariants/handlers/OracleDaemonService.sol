2d9a960cf4631c81daaf81908202b6c54b7e9dd3
iamsahu <praffulsahu@gmail.com>
2023-11-09 10:50:26 +0100

    feat: oracle service

---
+++ contracts/test/invariants/handlers/OracleDaemonService.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import "forge-std/console.sol";
+import "forge-std/StdInvariant.sol";
+
+import {Base} from "../Base.sol";
+import {BaseService} from "./BaseService.sol";
+
+import {IOracleManagerV1} from "../../../src/interfaces/components/IOracleManager.1.sol";
+
+contract OracleDaemonService is BaseService {
+    constructor(Base _base) BaseService(_base) {}
+
+    function getTargetSelectors() external view override returns (StdInvariant.FuzzSelector memory selectors) {
+        bytes4[] memory selectorsArray = new bytes4[](1);
+        selectorsArray[0] = this.action_dummy_report.selector;
+
+        selectors.selectors = selectorsArray;
+        selectors.addr = address(this);
+    }
+
+    function action_dummy_report() external prankOracleMember recordBlockData {
+        console.log("Dummy report");
+        IOracleManagerV1.ConsensusLayerReport memory dummyReport;
+        dummyReport.stoppedValidatorCountPerOperator = new uint32[](1);
+        base.oracle().reportConsensusLayerData(dummyReport);
+    }
+
+    // function action_report() external prankOracleMember() recordBlockData {
+    //     // Do we have a check about whether the report is going after the correct amount of duration
+    //     // Do we call time skip to make the report valid?
+    //     // Calculate epoch
+    //     // Get validators balance
+    //     // Get validators skimmed balance
+    //     // Get validators exited balance
+    //     // Get validators exiting balance
+    //     // Get validators count
+    //     // Get Stopped validator count per operator
+    // }
+}
