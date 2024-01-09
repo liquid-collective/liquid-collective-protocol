701282bf0699919135d5cde31d2591c43b0817f3
Tadeas Kucera <tadeas@certora.com>
2024-01-09 13:48:46 +0100

    Add everything to munged

--- certora/harness/RiverV1Harness.sol
+++ certora/harness/RiverV1Harness.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.20;
 
-import "../munged/River.1.sol";
+import "../munged/contracts/src/River.1.sol";
 
 contract RiverV1Harness is RiverV1 {
 }
