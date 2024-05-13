259243358c5c3f6e0c7ec5d08e934c3eb9d1c9be
iamsahu <praffulsahu@gmail.com>
2024-05-13 11:34:05 +0200

    fix: added tests for active operators count

--- contracts/test/OperatorsRegistry.1.t.sol
+++ contracts/test/OperatorsRegistry.1.t.sol
@@ -764,6 +764,7 @@ contract OperatorsRegistryV1Tests is OperatorsRegistryV1TestBase, BytesGenerator
     }
 
     function testGetAllActiveOperators(bytes32 _name, uint256 _firstAddressSalt, uint256 _count) public {
+        vm.assume(_count < 100_000);
         address[] memory _firstAddress = new address[](_count);
         _firstAddress = uf._newMulti(_firstAddressSalt, _count);
         vm.startPrank(admin);
@@ -783,6 +784,7 @@ contract OperatorsRegistryV1Tests is OperatorsRegistryV1TestBase, BytesGenerator
     function testGetAllActiveOperatorsWithInactiveOnes(bytes32 _name, uint256 _firstAddressSalt, uint256 _count)
         public
     {
+        vm.assume(_count < 100_000);
         address[] memory _firstAddress = new address[](_count);
         _firstAddress = uf._newMulti(_firstAddressSalt, _count);
         for (uint256 i; i < _count; i++) {
