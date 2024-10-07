3f8ffb3f75895318e64f0c062374391d51572028
Aleksander Kryukov <firealexkryukov@gmail.com>
2024-10-07 03:15:49 +0100

    CI first phase

--- runCI_confs.sh
+++ runCI_confs.sh
@@ -1,7 +1,53 @@
-#certoraRun.py certora/confs_for_CI/AllowlistV1.conf          # https://prover.certora.com/output/6893/6642425d43b04688b9bb4a69f8f9296f/?anonymousKey=472e7619ff28129838f90816f0f106ff15163acc
-#certoraRun.py certora/confs_for_CI/RiverV1.conf              # https://prover.certora.com/output/6893/ee61ca3e6a924f7493e4d43233b8d2dd/?anonymousKey=eb318e4ef87bb7273864d22af7392b7cc5cc5293
-#certoraRun.py certora/confs_for_CI/SharesManagerV1.conf      # https://prover.certora.com/output/6893/0fe2f29bd89644d382731a254c1aa433/?anonymousKey=9a690418ceef96e80fd43403343234ef1515d252
-#certoraRun.py certora/confs_for_CI/RedeemManagerV1.conf      # https://prover.certora.com/output/6893/ca950da0597b438fa9205799dd3417e0/?anonymousKey=fb65fd603471bb5752148f8f5c9988060862fda6
-#certoraRun.py certora/confs_for_CI/OperatorRegistryV1_1.conf # https://prover.certora.com/output/6893/20a05c289aa1498b8782e2272b2b5218/?anonymousKey=2457a353d6f59f5fe699276fd331b209207c5bdf
-#certoraRun.py certora/confs_for_CI/OperatorRegistryV1_2.conf # https://prover.certora.com/output/6893/9bb94ee9d9364a1b90072f6be0504c22/?anonymousKey=8af39f6937dc8c8f54d1a98b5c680addcbd9b59c
-#certoraRun.py certora/confs_for_CI/OperatorRegistryV1_3.conf # 
+# certoraRun certora/confs_for_CI/AllowlistV1.conf          # https://prover.certora.com/output/6893/6642425d43b04688b9bb4a69f8f9296f/?anonymousKey=472e7619ff28129838f90816f0f106ff15163acc
+# certoraRun certora/confs_for_CI/RiverV1.conf              # https://prover.certora.com/output/6893/ee61ca3e6a924f7493e4d43233b8d2dd/?anonymousKey=eb318e4ef87bb7273864d22af7392b7cc5cc5293
+# certoraRun certora/confs_for_CI/SharesManagerV1.conf      # https://prover.certora.com/output/6893/0fe2f29bd89644d382731a254c1aa433/?anonymousKey=9a690418ceef96e80fd43403343234ef1515d252
+# certoraRun certora/confs_for_CI/RedeemManagerV1.conf      # https://prover.certora.com/output/6893/ca950da0597b438fa9205799dd3417e0/?anonymousKey=fb65fd603471bb5752148f8f5c9988060862fda6
+
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_1.conf
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_2.conf
+
+
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_31.conf
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_32.conf # good with 2 loops: https://prover.certora.com/output/3106/b5e3055cb54e492bacefe0e267c7780f/?anonymousKey=667e9fb4231e637553c5fd44812d02d6322df8d2
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_33.conf
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_34.conf # good with 2 loops: https://prover.certora.com/output/3106/4bb378277da4449fa9062b4ec858a6d0/?anonymousKey=a36c270ecae5921b89aa562f93c9f074fc534e87
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_35.conf
+
+
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule exitingValidatorsDecreasesDiscrepancy --msg "exitingValidatorsDecreasesDiscrepancy"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule fundedAndExitedCanOnlyIncrease_IL2 --msg "fundedAndExitedCanOnlyIncrease_IL2"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule fundedAndExitedCanOnlyIncrease_IL4 --msg "fundedAndExitedCanOnlyIncrease_IL4"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule fundedAndExitedCanOnlyIncrease_removeValidators --msg "fundedAndExitedCanOnlyIncrease_removeValidators"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule fundedKeysCantBeChanged --msg "fundedKeysCantBeChanged"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule fundedValidatorCantBeRemoved --msg "fundedValidatorCantBeRemoved"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule removeValidatorsDecreaseKeys --msg "removeValidatorsDecreaseKeys"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule startingValidatorsDecreasesDiscrepancy --msg "startingValidatorsDecreasesDiscrepancy"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_0in_M9 --msg "validatorStateTransition_0in_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_0in_index --msg "validatorStateTransition_0in_index"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_1in_M9 --msg "validatorStateTransition_1in_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_1in_index --msg "validatorStateTransition_1in_index"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_2_1_index_limit --msg "validatorStateTransition_2_1_index_limit"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_2in_M9 --msg "validatorStateTransition_2in_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_2in_index --msg "validatorStateTransition_2in_index"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3_2 --msg "validatorStateTransition_3_2"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3_4_M10 --msg "validatorStateTransition_3_4_M10"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3_4_M12 --msg "validatorStateTransition_3_4_M12"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3_4_M14 --msg "validatorStateTransition_3_4_M14"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3_4_M7 --msg "validatorStateTransition_3_4_M7"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3_4_M9 --msg "validatorStateTransition_3_4_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3in_M9 --msg "validatorStateTransition_3in_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_3in_index --msg "validatorStateTransition_3in_index"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M10 --msg "validatorStateTransition_4_3_M10"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M12 --msg "validatorStateTransition_4_3_M12"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M13 --msg "validatorStateTransition_4_3_M13"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M14 --msg "validatorStateTransition_4_3_M14"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M15 --msg "validatorStateTransition_4_3_M15"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M7 --msg "validatorStateTransition_4_3_M7"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4_3_M9 --msg "validatorStateTransition_4_3_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4in_M9 --msg "validatorStateTransition_4in_M9"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule validatorStateTransition_4in_index --msg "validatorStateTransition_4in_index"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule whoCanChangeOperatorsCount_IL2 --msg "whoCanChangeOperatorsCount_IL2"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule whoCanChangeValidatorsCount --msg "whoCanChangeValidatorsCount"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule whoCanDeactivateOperator_LI2 --msg "whoCanDeactivateOperator_LI2"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule witness4_3ExitingValidatorsDecreasesDiscrepancy --msg "witness4_3ExitingValidatorsDecreasesDiscrepancy"
+# certoraRun certora/confs_for_CI/OperatorRegistryV1_3.conf --rule witness4_3StartingValidatorsDecreasesDiscrepancy --msg "witness4_3StartingValidatorsDecreasesDiscrepancy"
