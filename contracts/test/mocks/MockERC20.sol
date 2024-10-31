104aac8edec857e613ed9805b4c6515243fc4fc6
juliaaschmidt <drjuliaalexandraschmidt@gmail.com>
2024-10-31 14:19:15 +0000

    style-fix: linter error

--- contracts/test/mocks/MockERC20.sol
+++ contracts/test/mocks/MockERC20.sol
@@ -6,8 +6,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 contract MockERC20 is ERC20 {
     bool transferFromFail;
 
-    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol) {
-    }
+    constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol) {}
 
     function setTransferFromFail(bool _fail) external {
         transferFromFail = _fail;
