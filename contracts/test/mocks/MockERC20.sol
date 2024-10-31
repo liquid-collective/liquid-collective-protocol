e6bbc75f3abed09e2ed03b25d3ac315d888b9ca6
juliaaschmidt <drjuliaalexandraschmidt@gmail.com>
2024-10-31 14:16:01 +0000

    style: cleanup

--- contracts/test/mocks/MockERC20.sol
+++ contracts/test/mocks/MockERC20.sol
@@ -1,15 +1,12 @@
-// MockERC20.sol
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     bool transferFromFail;
 
     constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol) {
-        // No need to set decimals here, OpenZeppelin's ERC20 defaults to 18 decimals
     }
 
     function setTransferFromFail(bool _fail) external {
