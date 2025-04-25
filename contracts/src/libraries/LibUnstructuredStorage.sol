3357b27a6a8cb8ff33feec977a923a3d685bbc35
Mischa <mischa@mmt.me.uk>
2025-04-25 11:20:23 +0100

    This makes the libraries usuable by any ^0.8.20 contracts

--- contracts/src/libraries/LibUnstructuredStorage.sol
+++ contracts/src/libraries/LibUnstructuredStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 /// @title Lib Unstructured Storage
 /// @notice Utilities to work with unstructured storage
