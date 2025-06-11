59b318292ac004ca3c1cd2aca73dbd63a6c44a15
Mehdi Bechiri <cebidhem@pm.me>
2025-06-11 14:35:57 +0200

    chore: remove holesky artifacts and references
    Signed-off-by: Mehdi Bechiri <cebidhem@pm.me>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -38,16 +38,6 @@ const config: HardhatUserConfig = {
       url: process.env.RPC_URL || "",
       accounts: [process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000"],
     },
-    holesky: {
-      url: process.env.RPC_URL || "",
-      accounts: [process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000"],
-      gas: 5000000,
-    },
-    devHolesky: {
-      url: process.env.RPC_URL || "",
-      accounts: [process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000"],
-      gas: 5000000,
-    },
     hardhat: {
       accounts: {
         mnemonic: "word word word word word word word word word word word word",
@@ -75,8 +65,6 @@ const config: HardhatUserConfig = {
       default: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
       local: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
       mainnet: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
-      holesky: "0x4242424242424242424242424242424242424242",
-      devHolesky: "0x4242424242424242424242424242424242424242",
       tenderly: "0x4242424242424242424242424242424242424242",
       hoodi: "0x00000000219ab540356cBB839Cbe05303d7705Fa",
     },
@@ -84,8 +72,6 @@ const config: HardhatUserConfig = {
       default: 1,
       local: "0x71c9DAb681C209bb82270906e3B49388b2C15404",
       mainnet: "0xE3208Aa9d1186c1D1C8A5b76E794b2B68E6cb3a5",
-      holesky: "0x9F84E1a8749D331C68Fb0322C9E24a5FB3334398",
-      devHolesky: "0x0e9eAd2FEB500DB46E6EB95b352FA4a86aC13dBE",
       tenderly: "0x0e9eAd2FEB500DB46E6EB95b352FA4a86aC13dBE",
       hoodi: "0xF733B0eCf2141db2956d8Ea9ab98e5Cc33CA2f80",
     },
@@ -93,8 +79,6 @@ const config: HardhatUserConfig = {
       default: 1,
       local: "0x71c9DAb681C209bb82270906e3B49388b2C15404",
       mainnet: "0xDE55C9dc78f985fE1502484Cb98EBfAB66A56B62",
-      holesky: "0xE22F86Be928E03D50411C588d689C0f33900bb4c",
-      devHolesky: "0xe953E4df3dDd575D2C1E1950ec4Fa33CF89947DA",
       tenderly: "0xe953E4df3dDd575D2C1E1950ec4Fa33CF89947DA",
       hoodi: "0xF733B0eCf2141db2956d8Ea9ab98e5Cc33CA2f80",
     },
@@ -102,8 +86,6 @@ const config: HardhatUserConfig = {
       default: 2,
       local: "0x07706A7D768054c10eB4FC9103Ea322f62831cb9",
       mainnet: "0x8EE3fC0Bcd7B57429203751C5bE5fdf1AB8409f3",
-      holesky: "0x80Cf8bD4abf6C078C313f72588720AB86d45c5E6",
-      devHolesky: "0x0FdEe4562D7e6dbA05A9f892D2Be04B83f3E7579",
       tenderly: "0x8EE3fC0Bcd7B57429203751C5bE5fdf1AB8409f3",
       hoodi: "0xF733B0eCf2141db2956d8Ea9ab98e5Cc33CA2f80",
     },
@@ -111,8 +93,6 @@ const config: HardhatUserConfig = {
       default: 1,
       local: "0x71c9DAb681C209bb82270906e3B49388b2C15404",
       mainnet: "0xE3208Aa9d1186c1D1C8A5b76E794b2B68E6cb3a5",
-      holesky: "0x47f049e943ABFbd27Bb11aF3195FEc153A28598b",
-      devHolesky: "0xc5DB3C539900B1A2889c37BEaE789D0EB57e8681",
       tenderly: "0xc5DB3C539900B1A2889c37BEaE789D0EB57e8681",
       hoodi: "0xF733B0eCf2141db2956d8Ea9ab98e5Cc33CA2f80",
     },
@@ -120,8 +100,6 @@ const config: HardhatUserConfig = {
       default: 1,
       local: "0x7932EdA85E33D8e13f7C110ACBEb4a5A8B53dda9",
       mainnet: "0x070cbF96cac223D88401D6227577f9FA480C57C8",
-      holesky: "0xb85f6480A2BffF946Ca1874ad6E2bB55a4CF5059", // EOA
-      devHolesky: "0x67AB27C56cDB02C6c0f8B89948350Ebbb1837577", // EOA
       tenderly: "0x67AB27C56cDB02C6c0f8B89948350Ebbb1837577", // EOA
     },
   },
@@ -132,14 +110,6 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || "",
     customChains: [
-      {
-        network: "holesky",
-        chainId: 17000,
-        urls: {
-          apiURL: "https://api-holesky.etherscan.io/api",
-          browserURL: "https://holesky.etherscan.io",
-        },
-      },
       {
         network: "hoodi",
         chainId: 560048,
