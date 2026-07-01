fa18781e980c5568abc802d1dc92df57c964ae8a
Mischa <mischa@mmt.me.uk>
2026-07-01 11:15:23 +0100

    Update contracts/src/OperatorsRegistry.1.sol
    Signed-off-by: Mischa <mischa@mmt.me.uk>

--- contracts/src/OperatorsRegistry.1.sol
+++ contracts/src/OperatorsRegistry.1.sol
@@ -424,7 +424,7 @@ contract OperatorsRegistryV1 is IOperatorsRegistryV1, Initializable, Administrab
     /// @notice Validates a single operator's EL exit allocation, reserves it, triggers the withdrawal and emits.
     /// @return elExitAmount The total reserved exit amount (wei) for this operator
     /// @return feePaid The withdrawal fee paid for this operator
-    function _requestSingleELETHExit(
+    function _requestELETHExits(
         IWithdrawV1 _withdraw,
         ELExitETHAllocation calldata _operatorAllocation,
         uint256 _maxFeePerWithdrawal
