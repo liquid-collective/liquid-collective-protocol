10333a333412e3b8a1de0000a66714ff5b175861
juliaaschmidt <121238805+juliaaschmidt@users.noreply.github.com>
2026-07-03 11:56:05 +0100

    fix: standardize consolidation recipient resolution (BS-4274) (#633)
    * fix: standardize consolidation recipient resolution
    
    Refs #556
    
    * fix(review) natspec fixes
    
    ---------
    
    Co-authored-by: Mischa <mischa@mmt.me.uk>

--- contracts/src/state/externalConsolidationRecipientMapping/ExternalConsolidationRecipientMapping.sol
+++ contracts/src/state/externalConsolidationRecipientMapping/ExternalConsolidationRecipientMapping.sol
@@ -2,22 +2,22 @@
 pragma solidity 0.8.34;
 
 /// @title External Consolidation Recipient Mapping Storage
-/// @notice Utility to manage withdrawal credential recipient mapping storage
+/// @notice Utility to manage withdrawal credentials to recipient address mapping storage
 library ExternalConsolidationRecipientMapping {
-    /// @notice Storage slot of the withdrawal credential recipient mapping
+    /// @notice Storage slot of the withdrawal-credential-address recipient mapping
     bytes32 internal constant EXTERNAL_CONSOLIDATION_RECIPIENT_MAPPING_SLOT =
         bytes32(uint256(keccak256("river.state.externalConsolidationRecipientMapping")) - 1);
 
     /// @notice Structure stored in storage slot
     struct Slot {
-        /// @custom:attribute Mapping from withdrawal credential addresses to recipient addresses
+        /// @custom:attribute Mapping from withdrawal credentials to recipient addresses
         mapping(address => address) value;
     }
 
     /// @notice Retrieve the recipient mapped to a withdrawal credential address
-    /// @param _withdrawalCredential The withdrawal credential address to query
+    /// @param _withdrawalCredentialAddress The withdrawal credential address to query
     /// @return The mapped recipient address
-    function get(address _withdrawalCredential) internal view returns (address) {
+    function get(address _withdrawalCredentialAddress) internal view returns (address) {
         bytes32 slot = EXTERNAL_CONSOLIDATION_RECIPIENT_MAPPING_SLOT;
 
         Slot storage r;
@@ -27,13 +27,13 @@ library ExternalConsolidationRecipientMapping {
             r.slot := slot
         }
 
-        return r.value[_withdrawalCredential];
+        return r.value[_withdrawalCredentialAddress];
     }
 
     /// @notice Set the recipient mapped to a withdrawal credential address
-    /// @param _withdrawalCredential The withdrawal credential address to update
+    /// @param _withdrawalCredentialAddress The withdrawal credential address to update
     /// @param _recipient The recipient address to set
-    function set(address _withdrawalCredential, address _recipient) internal {
+    function set(address _withdrawalCredentialAddress, address _recipient) internal {
         bytes32 slot = EXTERNAL_CONSOLIDATION_RECIPIENT_MAPPING_SLOT;
 
         Slot storage r;
@@ -43,6 +43,6 @@ library ExternalConsolidationRecipientMapping {
             r.slot := slot
         }
 
-        r.value[_withdrawalCredential] = _recipient;
+        r.value[_withdrawalCredentialAddress] = _recipient;
     }
 }
